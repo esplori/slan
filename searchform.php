f5d9a75fe6fc1971e5117ec70365a10ec1b9e627
vistol <2381773226@qq.com>
2020-10-12 22:28:13 +0800

    update search btn value

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
 	<div>
 		<input class="search-text" type="text" value="<?php the_search_query(); ?>" name="s" id="s" />
-		<input class="search-btn" type="submit" id="searchsubmit" value="search" />
+		<input class="search-btn" type="submit" id="searchsubmit" value="搜索" />
 	</div>
 </form>
